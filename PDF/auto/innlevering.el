743f1a5c33885ade68c9ad70d713f1cd4e4c35ca
Håvar Eggereide <havar.eggereide@student.uib.no>
2016-11-14 20:02:31 +0100

    små endringer

--- PDF/auto/innlevering.el
+++ PDF/auto/innlevering.el
@@ -3,10 +3,10 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "a4paper") ("biblatex" "backend=bibtex" "style=verbose-trad2") ("inputenc" "utf8")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "report"
